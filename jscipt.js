311aed8c30542061c51873f7dc11e82b91448169
Aanghell <andrei.anghell210@gmail.com>
2025-03-06 19:17:53 +0200

    Computer picks + winning conditions
    Added the function of computer to pick a number from 1 to 3, each one being assigned a class - rock (1); paper (2) and scissors (3).
    
    Assigned the draw, winning and losing conditions - each of them informing the player via alert and console.log on the result.

--- jscipt.js
+++ jscipt.js
@@ -7,8 +7,13 @@ console.log("Welcome to my 'Rock - Paper - Scissors' minigame!")
 // - ex: if user: rock && comp: paper => user lost , 
 // else if comp: rock => try again, else => user won
 
-let userOptions = ["rock","paper","scissors"];
+
+//user input//
+let userOptions = ["Rock","Paper","Scissors"];
+
 userInput=prompt("Welcome to ROCK PAPER SCISSORS! Make your choice!");
+
+userInput = userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
 
 while(userInput === "" || userInput === null){
     userInput =prompt("Please make your choice");
@@ -16,8 +21,49 @@ while(userInput === "" || userInput === null){
 }
 while (!userOptions.includes(userInput)){
     userInput= prompt("Wrong choice, try again!")
-}
+};
 alert ("You chose " + userInput+ ".");
 
+//ai choice
+function getComputerChoice(){
+    return Math.floor (Math.random() * 3)+1;
+};
 
+let aiChoiceNumber = getComputerChoice();
 
+let aiChoiceString = "";
+
+if (aiChoiceNumber === 1) {
+aiChoiceString = "Rock"
+} 
+else if (aiChoiceNumber === 2) {
+    aiChoiceString = "Paper"
+}
+else {
+    aiChoiceString = "Scissors"
+}
+console.log("Computer chose " +aiChoiceString + ".")
+
+//deciding factors
+
+//draw
+
+if (userInput === aiChoiceString) {
+    console.log("Draw! You both picked the same thing.")
+}
+
+//winning conditions
+
+else if ((userInput === "Rock" && aiChoiceString === "Scissor") || 
+        (userInput === "Paper" && aiChoiceString === "Rock") || 
+        (userInput === "Scissors" && aiChoiceString === "Rock")) {
+        console.log("You won!");
+        alert("You won! Computer picked "+ aiChoiceString + ".")
+    }
+
+//losing conditions
+
+else {
+    console.log ("You lost!")
+    alert("You lost! Computer picked "+ aiChoiceString + ".")
+};
