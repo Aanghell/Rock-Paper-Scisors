5d47b382f8fbce48fafed4f04534ef3128edde11
Aanghell <andrei.anghell210@gmail.com>
2025-03-06 18:13:56 +0200

    Prompt + loop options
    Designed the prompt in such way that the user has to make a choice.
    User is also forced to only make a choice between "rock" "paper and "scissors".
    Both of these operations were made using the "while" statement - creating a loop.

--- jscipt.js
+++ jscipt.js
@@ -1,2 +1,23 @@
 //ROCK PAPER SCISORS PROJECT//
 console.log("Welcome to my 'Rock - Paper - Scissors' minigame!")
+
+//User will choose via prompt one of 'Rock - Paper - Scissors'
+//Computer will be asigned a random one 
+//We shall have 3 functions - one for each user's choise: 
+// - ex: if user: rock && comp: paper => user lost , 
+// else if comp: rock => try again, else => user won
+
+let userOptions = ["rock","paper","scissors"];
+userInput=prompt("Welcome to ROCK PAPER SCISSORS! Make your choice!");
+
+while(userInput === "" || userInput === null){
+    userInput =prompt("Please make your choice");
+    
+}
+while (!userOptions.includes(userInput)){
+    userInput= prompt("Wrong choice, try again!")
+}
+alert ("You chose " + userInput+ ".");
+
+
+
