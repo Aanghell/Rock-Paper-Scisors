13e2b1bd88fd3851a4aa9fc231d7b2a73285ff1a
Aanghell <andrei.anghell210@gmail.com>
2025-03-07 17:53:09 +0200

    Added a 5 rounds system
    Added a feature that allows the user to play 5 rounds, after which the winner is determined.

--- jscipt.js
+++ jscipt.js
@@ -8,21 +8,50 @@ console.log("Welcome to my 'Rock - Paper - Scissors' minigame!")
 // else if comp: rock => try again, else => user won
 
 
-//user input//
 let userOptions = ["Rock","Paper","Scissors"];
 
+//the whole game
+
+function playGame (){
+
+    let userScore= 0;
+
+    let aiScore = 0;
+
+    
+
+for (let i=0; i<5; i++){
+    let roundResult = playRound();
+    if (roundResult === "win") {
+        userScore++
+    }
+    else if (roundResult === "lose") {
+        aiScore++
+    }
+}
+    if (userScore>aiScore) {
+        alert("Player score: " + userScore +" \nComputer score: "+ aiScore +". \nYou won!")}
+        else if (userScore<aiScore) {
+            alert("Player score: " + userScore +" \nComputer score: "+ aiScore +". \nYou lost!");
+        }
+        else {
+            alert("Player score: " + userScore +" \nComputer score: "+ aiScore +". \nIt's a draw!");
+        }
+    }
+
+//each round
+
+function playRound(userInput) {
 userInput=prompt("Welcome to ROCK PAPER SCISSORS! Make your choice!");
 
 userInput = userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
 
 while(userInput === "" || userInput === null){
     userInput =prompt("Please make your choice");
-    
 }
 while (!userOptions.includes(userInput)){
     userInput= prompt("Wrong choice, try again!")
 };
-alert ("You chose " + userInput+ ".");
 
 //ai choice
 function getComputerChoice(){
@@ -30,7 +59,6 @@ function getComputerChoice(){
 };
 
 let aiChoiceNumber = getComputerChoice();
-
 let aiChoiceString = "";
 
 if (aiChoiceNumber === 1) {
@@ -42,28 +70,39 @@ else if (aiChoiceNumber === 2) {
 else {
     aiChoiceString = "Scissors"
 }
-console.log("Computer chose " +aiChoiceString + ".")
+console.log("You chose " + userInput + ". " + "Computer chose " +aiChoiceString + ".");
 
 //deciding factors
-
+let roundResult= ""
 //draw
 
 if (userInput === aiChoiceString) {
+    alert("You chose " + userInput + "." + "\nComputer chose " +aiChoiceString + "." +"\nIt's a draw!")
     console.log("Draw! You both picked the same thing.")
+    roundResult = "draw"
 }
 
 //winning conditions
 
-else if ((userInput === "Rock" && aiChoiceString === "Scissor") || 
+else if ((userInput === "Rock" && aiChoiceString === "Scissors") || 
         (userInput === "Paper" && aiChoiceString === "Rock") || 
         (userInput === "Scissors" && aiChoiceString === "Rock")) {
+            alert("You chose " + userInput + "." + "\nComputer chose " +aiChoiceString + "." +"\nYou won this round!")
         console.log("You won!");
-        alert("You won! Computer picked "+ aiChoiceString + ".")
+        roundResult = "win"
+
     }
 
 //losing conditions
 
 else {
-    console.log ("You lost!")
-    alert("You lost! Computer picked "+ aiChoiceString + ".")
+    alert("You chose " + userInput + "." + "\nComputer chose " +aiChoiceString + "." +"\nYou lost this round!")
+    console.log("You lost!" + "\nThe score is:\nPlayer: " + userScore + "\nComputer: " + aiScore);
+    roundResult = "lose";
+    
 };
+return roundResult
+}
+playGame()
+
+//FINALLY!!!
